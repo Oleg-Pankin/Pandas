076027a1acf3d10f66499b897bfe21afb29cc1ff
Олег <alexpankin1972@gmail.com>
2022-01-28 12:40:31 +0500

    pochti

--- mean.py
+++ mean.py
@@ -1,12 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from collections import Counter
 df = pd.read_csv('Games.csv')
-print(df['Sales'].min())
-print(df['Sales'].max())
-print(df['Sales'].mean())
-print(df['Sales'].median())
-print(df[df['Sales']>41]['Name'])
-print(df[df['Sales']<2]['Name'])
-print(df[df['Publisher']=='Electronic Arts']['Name'])
-print(df[df['Release']=='Jun-09']['Name'])
-for data in df['Release']:
+counter = {}
+counter = Counter(df['Release'])
+res = [k for k in counter if counter[k]>2]
+for something in res:
+    print(df[df['Release'] == something]['Name'],something)
+
+#print(df['Sales'].min())
+#print(df['Sales'].max())
+#print(df['Sales'].mean())
+#print(df['Sales'].median())
+#print(df[df['Sales']>41]['Name'])
+#print(df[df['Sales']<2]['Name'])
+#print(df[df['Publisher']=='Electronic Arts']['Name'])
+#print(df[df['Release']=='Jun-09']['Name'])
