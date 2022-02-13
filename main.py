7f76bd0bd08f5d2b78e686620fe8006e181ee347
toha-pixel <tohich2006@gmail.com>
2022-02-13 16:28:56 +0500

    vdrug prigoditsya1

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv('Games.csv')
-df.plot(y = 'Sales', kind='bar')
+df = pd.read_csv('MPR.csv')
+print(df[df['camera']==(df['camera'].max())]['model'])
+print(df[df['selfie']==(df['selfie'].max())]['model'])
+print(df[df['audio']==(df['audio'].max())]['model'])
+print(df[df['display']==(df['display'].max())]['model'])
+print(df[df['battery']==(df['battery'].max())]['model'])
+def convert(data):
+    data.replace('$','')
+    return int(data)
+df['price']=df['price'].apply(convert())
+print(df[df['price']==(df['price'].mean())]['model'])
