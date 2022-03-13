70f8213e1b6dd8b59b1fcf17faa538c2c9c5170b
toha-pixel <tohich2006@gmail.com>
2022-03-13 16:35:54 +0500

    vdrug prigoditsya1

--- Error correction.py
+++ Error correction.py
@@ -1,5 +1,6 @@
 import pandas as pd
-df = pd.read_csv('LOL Stats 12.1.csv',sep = ';')
+from collections import Counter
+df = pd.read_csv('Games.csv')
 print(df.info())
-df.fillna('n',inplace = True)
-print(df.info())
+print(df['Sales'].mean())
+print(df[df['Sales']>1.5]['Genre'])
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 df = pd.read_csv('MPR.csv')
-print(df[df['camera']==(df['camera'].max())]['model'])
-print(df[df['selfie']==(df['selfie'].max())]['model'])
-print(df[df['audio']==(df['audio'].max())]['model'])
-print(df[df['display']==(df['display'].max())]['model'])
-print(df[df['battery']==(df['battery'].max())]['model'])
+print(df[df['camera'] == (df['camera'].max())]['model'])
+print(df[df['selfie'] == (df['selfie'].max())]['model'])
+print(df[df['audio'] == (df['audio'].max())]['model'])
+print(df[df['display'] == (df['display'].max())]['model'])
+print(df[df['battery'] == (df['battery'].max())]['model'])
+
+
 def convert(data):
-    data.replace('$','')
+    data.replace('$', '')
     return int(data)
-df['price']=df['price'].apply(convert())
-print(df[df['price']==(df['price'].mean())]['model'])
+
+
+df['price'] = df['price'].apply(convert)
+print(df[df['price'] == (df['price'].mean())]['model'])
+
